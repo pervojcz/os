d0ca1f96f6299f78963536f721ce8429ee76d0ab
Vojtěch Perník <develop@pervoj.cz>
2026-02-03 13:44:47 +0100

    fix: install bitwarden package manually

--- variants/circle/scripts/bitwarden.ts
+++ variants/circle/scripts/bitwarden.ts
@@ -1,3 +1,5 @@
+import { $ } from "bun";
+import { join } from "path";
 import type { VariantCtx } from "~/utils/create-variant";
 
 export async function installBitwarden(ctx: VariantCtx) {
@@ -10,9 +12,18 @@ export async function installBitwarden(ctx: VariantCtx) {
   const rpm = assets.find((a) => a.name.endsWith(`-${ctx.architecture}.rpm`));
   if (!rpm) throw new Error("No Bitwarden desktop .rpm found");
 
-  // const tempDir = ctx.getTempDir("bitwarden", rpm.name);
-  // const rpmFile = join(tempDir, rpm.name);
+  const tempDir = ctx.getTempDir("bitwarden", rpm.name);
+  const rpmFile = join(tempDir, rpm.name);
+  const extractedDir = join(tempDir, "output");
 
-  // await ctx.downloadFile(rpm.url, rpmFile);
-  await ctx.installPackages(rpm.url);
+  await ctx.downloadFile(rpm.url, rpmFile);
+  await $`rpm2cpio ${rpmFile} | cpio -idmv -D ${extractedDir}`.quiet();
+
+  await $`mv ${extractedDir}/opt/Bitwarden ${extractedDir}/usr/share/bitwarden`;
+  await $`rm -rf ${extractedDir}/opt`;
+
+  await $`sed -i 's|/opt/Bitwarden/bitwarden|/usr/share/bitwarden/bitwarden|g' ${extractedDir}/usr/share/applications/bitwarden.desktop`;
+
+  await ctx.copyFiles(extractedDir);
+  await $`ln -s /usr/share/bitwarden/bitwarden /usr/bin/bitwarden`;
 }
