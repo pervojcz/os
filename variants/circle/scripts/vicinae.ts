cf5b62af0caff7f1e733db09f50a7cc8c7e176c7
Vojtěch Perník <develop@pervoj.cz>
2026-08-22 13:25:55 +0300

    fix: vicinae gnome extension installation

--- variants/circle/scripts/vicinae.ts
+++ variants/circle/scripts/vicinae.ts
@@ -12,8 +12,9 @@ export const getVicinaeTask = createTaskGetter(async (ctx) => {
   const extensionAsset = assets.find((a) =>
     a.name.includes(".shell-extension"),
   );
-  if (!extensionAsset)
+  if (!extensionAsset) {
     throw new Error("Vicinae GNOME extension asset not found");
+  }
 
   const tempDir = ctx.getTempDir(
     "vicinae-gnome-extension",
@@ -22,9 +23,11 @@ export const getVicinaeTask = createTaskGetter(async (ctx) => {
   const zipFile = join(tempDir, extensionAsset.name);
   await ctx.downloadFile(extensionAsset.url, zipFile);
 
-  const extensionDir = "/usr/share/gnome-shell/extensions/vicinae@dagimg-dot";
-  await $`mkdir -p ${extensionDir}`;
-  await $`unzip -o ${zipFile} -d ${extensionDir}`;
+  await $`gnome-extensions install --force ${zipFile}`;
+
+  // const extensionDir = "/usr/share/gnome-shell/extensions/vicinae@dagimg-dot";
+  // await $`mkdir -p ${extensionDir}`;
+  // await $`unzip -o ${zipFile} -d ${extensionDir}`;
 
   await $`systemctl --global enable vicinae`;
 });
