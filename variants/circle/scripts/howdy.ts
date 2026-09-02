f1ab60c1930637636a35619985c6d2310ba9fe62
Vojtěch Perník <develop@pervoj.cz>
2026-09-02 19:01:10 +0200

    fix(circle): switch to older version of howdy

--- variants/circle/scripts/howdy.ts
+++ variants/circle/scripts/howdy.ts
@@ -1,6 +1,6 @@
 import tools from "~/tools";
 
 export async function installHowdy() {
-  await tools.repos.addRepositoryFromCopr("principis/howdy-beta");
+  await tools.repos.addRepositoryFromCopr("principis/howdy");
   await tools.packages.installPackages("howdy");
 }
