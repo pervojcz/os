9ec7aa649f82e279fa2c2b1354e2330dad3d7980
Vojtěch Perník <develop@pervoj.cz>
2026-09-02 12:46:59 +0200

    fix: use dnf5 for packages instead of rpm-ostree

--- src/tools/packages.ts
+++ src/tools/packages.ts
@@ -1,67 +1,20 @@
-import { $, type ShellError } from "bun";
-
-export type InstallPackagesOptions = {
-  idempotent?: boolean;
-  repos?: string[];
-};
+import { $ } from "bun";
 
 export async function isPackageInstalled(packageName: string) {
   const result = await $`rpm -q ${packageName}`.nothrow();
   return result.exitCode === 0;
 }
 
-function parseInstallArgs(args: (string | InstallPackagesOptions)[]) {
-  const packages: string[] = [];
-  let options: InstallPackagesOptions = {};
-
-  for (const arg of args) {
-    if (typeof arg === "string") {
-      packages.push(arg);
-      continue;
-    }
-
-    options = arg;
-  }
-
-  return { packages, options };
-}
-
-export async function installPackages(
-  ...args: (string | InstallPackagesOptions)[]
-) {
-  const { packages, options } = parseInstallArgs(args);
+export async function installPackages(...packages: string[]) {
   if (!packages.length) return;
   console.log("Installing packages:", ...packages);
-
-  const flags: string[] = [];
-  if (options.idempotent) flags.push("--idempotent");
-  if (options.repos?.length) {
-    flags.push("--disablerepo=*");
-    for (const repo of options.repos) {
-      flags.push(`--enablerepo=${repo}`);
-    }
-  }
-
-  try {
-    await $`rpm-ostree install ${flags} ${packages}`;
-  } catch (e) {
-    const error = e as ShellError;
-
-    console.error(
-      "Failed to install packages:",
-      error.message,
-      "\n",
-      String.fromCharCode(...error.stderr),
-    );
-
-    throw error;
-  }
+  await $`dnf5 -y install ${packages}`;
 }
 
 export async function uninstallPackages(...packages: string[]) {
   if (!packages.length) return;
   console.log("Uninstalling packages:", ...packages);
-  await $`rpm-ostree uninstall ${packages}`;
+  await $`dnf5 -y remove ${packages}`;
 }
 
 export async function replacePackages(remove: string[], install: string[]) {
@@ -72,5 +25,5 @@ export async function replacePackages(remove: string[], install: string[]) {
   }
 
   console.log("Replacing packages:", ...remove, "->", ...install);
-  await $`rpm-ostree override remove ${remove} --install ${install}`;
+  await $`dnf5 -y --allowerasing remove ${remove} install ${install}`;
 }
