62ee0ddbcc716f72ba762439637152c9a2f55018
Vojtěch Perník <develop@pervoj.cz>
2026-09-02 19:12:07 +0200

    fix(circle): switch from howdy back to authface

--- variants/circle/scripts/howdy.ts
+++
@@ -1,6 +0,0 @@
-import tools from "~/tools";
-
-export async function installHowdy() {
-  await tools.repos.addRepositoryFromCopr("principis/howdy");
-  await tools.packages.installPackages("howdy");
-}
