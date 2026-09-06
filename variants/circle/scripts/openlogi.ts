4d139c0775565bae54cc0a0e1f49c3a7b275c12f
Vojtěch Perník <develop@pervoj.cz>
2026-09-06 14:41:22 +0200

    fix: openlogi installation

--- variants/circle/scripts/openlogi.ts
+++ variants/circle/scripts/openlogi.ts
@@ -10,9 +10,9 @@ export async function installOpenLogi() {
   if (!rpm) throw new Error("OpenLogi RPM asset not found");
 
   const tempDir = await tools.files.createTmpDir("openlogi", rpm.name);
-  const archivePath = join(tempDir, rpm.name);
-  await tools.download.downloadFile(rpm.url, archivePath);
+  const rpmPath = join(tempDir, rpm.name);
+  await tools.download.downloadFile(rpm.url, rpmPath);
 
-  await tools.packages.installPackages(archivePath);
-  await $`systemctl enable openlogi-agent`;
+  await $`rpm --install --noscripts ${rpmPath}`;
+  await $`systemctl --global enable openlogi-agent`;
 }
