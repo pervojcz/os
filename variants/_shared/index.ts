f600ea3a6f6c9c7da4a3d7ab72e0c6cb65b696b2
Vojtěch Perník <develop@pervoj.cz>
2026-09-22 10:31:24 +0200

    fix: don't upgrade when building the containers

--- variants/_shared/index.ts
+++ variants/_shared/index.ts
@@ -1,10 +1,9 @@
-import tools from "~/tools";
 import { installCodecs } from "./codecs";
 import { installMiscPackages } from "./misc-packages";
 import { enableRpmfusion } from "./rpmfusion";
 
 export default async function runSharedScripts() {
-  await tools.packages.upgrade();
+  // await tools.packages.upgrade();
 
   await enableRpmfusion();
   await installMiscPackages();
